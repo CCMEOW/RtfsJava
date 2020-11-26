323a6e065e38d593a9c99c268d7be37170548b61
CCMEOW <ccmeowhappy@foxmail.com>
2020-11-26 17:50:59 +0800

    新建一个空分支

---
+++ src/test/cici/Main.java
@@ -0,0 +1,13 @@
+package test.cici;
+
+/**
+ * @author pengjiaqi
+ * @date 2020/10/22
+ * @description 初始测试类
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello cici");
+    }
+
+}
